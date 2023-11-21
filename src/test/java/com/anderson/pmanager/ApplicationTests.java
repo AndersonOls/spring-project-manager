2acfedf49d59e067155a7d1a998d56882dc4beb8
Anderson <anderson.silva1996@hotmail.com>
2023-11-21 18:45:28 -0300

    first commit

---
+++ src/test/java/com/anderson/pmanager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.pmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
